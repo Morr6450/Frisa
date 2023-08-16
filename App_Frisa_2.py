f513fb271b59cbce43e76a5dd81ae080baaf8f76
Morr6450 <134819974+Morr6450@users.noreply.github.com>
2023-08-16 16:09:02 -0700

    Update App_Frisa_2.py

--- App_Frisa_2.py
+++ App_Frisa_2.py
@@ -12,7 +12,16 @@ st.title(' :man-woman-boy-boy: 	:earth_americas: Fundacion Frisa')
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
 
-df = pd.read_csv('Prueba_de_datos.csv')
+fl = st.file_uploader(':file uploader: Sube un archivo',type=(["csv","txt","xlsx","xls"]))
+
+if fl is not None:
+    filename = fl.name
+    #st.write(filename)
+    df = pd.read_csv(filename)
+#else:
+    #Nombre del archivo dentro del GitHub
+    #df = pd.read_csv('Prueba_de_datos.csv')
+
 st.header('Archivo existente')
 st.write(df)
 
